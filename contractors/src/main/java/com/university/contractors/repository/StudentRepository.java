727c842197665e84372a2faa3752a219f3dbfbca
Oleg Barmin <olegbarmin@gmail.com>
2019-01-28 23:20:19 +0200

    Remove redundant method overwrite.

--- contractors/src/main/java/com/university/contractors/repository/StudentRepository.java
+++ contractors/src/main/java/com/university/contractors/repository/StudentRepository.java
@@ -7,12 +7,4 @@ import org.springframework.stereotype.Component;
 @Component
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
-    @Override
-    Iterable<Student> findAll();
-
-    @Override
-    <S extends Student> S save(S entity);
-
-    @Override
-    void deleteById(Long aLong);
 }
