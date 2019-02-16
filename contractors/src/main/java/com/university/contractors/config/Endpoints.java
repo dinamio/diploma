ea92451dafbacf270f1931041a842711bd83416e
Oleg Barmin <olegbarmin@gmail.com>
2019-02-16 21:48:39 +0200

    Add `Endpoints` interface to store constants with endpoints addresses.

---
+++ contractors/src/main/java/com/university/contractors/config/Endpoints.java
@@ -0,0 +1,10 @@
+package com.university.contractors.config;
+
+public interface Endpoints {
+
+    String STUDENTS = "/students";
+    String STUDENT_BY_ID = STUDENTS + "/{id}";
+
+    String LOGIN = "/login";
+
+}
