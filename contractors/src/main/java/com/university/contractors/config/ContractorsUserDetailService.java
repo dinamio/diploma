7e16f23f8cd93972272dd91eab64439202afb20e
Oleg Barmin <olegbarmin@gmail.com>
2019-02-17 23:10:45 +0200

    Rename 'CustomUserDetailService' to 'ContractorsUserDetailService'.

---
+++ contractors/src/main/java/com/university/contractors/config/ContractorsUserDetailService.java
@@ -0,0 +1,48 @@
+package com.university.contractors.config;
+
+import com.university.contractors.model.User;
+import com.university.contractors.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ContractorsUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ContractorsUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final Optional<User> userOptional = userRepository.findByUsername(username);
+
+        if (!userOptional.isPresent()) {
+            throw new UsernameNotFoundException(String.format("User with username '%s' was not found.", username));
+        }
+
+        final User user = userOptional.get();
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPasswordHash(),
+                AuthorityUtils.createAuthorityList(user.getUserRole().getValue()));
+
+    }
+
+    User loadCustomUserByUsername(String username) {
+        final Optional<User> userByUsername = userRepository.findByUsername(username);
+
+        if (!userByUsername.isPresent()) {
+            throw new UsernameNotFoundException(String.format("User with username '%s' was not found", username));
+        }
+
+        return userByUsername.get();
+    }
+}
