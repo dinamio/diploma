6708af38f51b7219755bd49088eced5b327262f3
Oleg Barmin <olegbarmin@gmail.com>
2019-02-17 23:45:34 +0200

    Update UserBuilder according to updated model.

--- contractors/src/test/java/com/university/contractors/model/UserBuilder.java
+++ contractors/src/test/java/com/university/contractors/model/UserBuilder.java
@@ -11,13 +11,28 @@ public final class UserBuilder {
         return new UserBuilder();
     }
 
+    public UserBuilder id(Long id) {
+        user.setId(id);
+        return this;
+    }
+
     public UserBuilder username(String username) {
         user.setUsername(username);
         return this;
     }
 
-    public UserBuilder password(String password) {
-        user.setPassword(password);
+    public UserBuilder passwordHash(String passwordHash) {
+        user.setPasswordHash(passwordHash);
+        return this;
+    }
+
+    public UserBuilder role(UserRole role) {
+        user.setUserRole(role);
+        return this;
+    }
+
+    public UserBuilder token(String token) {
+        user.setToken(token);
         return this;
     }
 
