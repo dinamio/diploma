b4d8b00bc5eb9a341e85e607364c8ba22fd8ce5b
Oleg Barmin <olegbarmin@gmail.com>
2019-02-17 23:30:18 +0200

    Replace @Component annotation with @Repository for better consistency.

--- contractors/src/main/java/com/university/contractors/repository/StudentRepository.java
+++ contractors/src/main/java/com/university/contractors/repository/StudentRepository.java
@@ -2,9 +2,9 @@ package com.university.contractors.repository;
 
 import com.university.contractors.model.Student;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
 }
