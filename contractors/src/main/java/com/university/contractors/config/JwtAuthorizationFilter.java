dfe4b13c9dfd8516cf9ca65819b1e10cb512ee20
Oleg Barmin <olegbarmin@gmail.com>
2019-02-17 23:20:11 +0200

    Refactor JwtAuthorizationFilter for better readability.

--- contractors/src/main/java/com/university/contractors/config/JwtAuthorizationFilter.java
+++ contractors/src/main/java/com/university/contractors/config/JwtAuthorizationFilter.java
@@ -2,38 +2,43 @@ package com.university.contractors.config;
 
 import com.university.contractors.model.User;
 import io.jsonwebtoken.Jwts;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 import static com.university.contractors.config.SecurityConstants.*;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
-    private final CustomUserDetailService customUserDetailService;
+    private final ContractorsUserDetailService contractorsUserDetailService;
 
-    JwtAuthorizationFilter(AuthenticationManager authenticationManager, CustomUserDetailService customUserDetailService) {
+    JwtAuthorizationFilter(AuthenticationManager authenticationManager, ContractorsUserDetailService contractorsUserDetailService) {
         super(authenticationManager);
-        this.customUserDetailService = customUserDetailService;
+        this.contractorsUserDetailService = contractorsUserDetailService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String header = request.getHeader(HEADER_NAME);
-        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+        final String authorizationHeaderValue = request.getHeader(HEADER_NAME);
+
+        if (hasInvalidHeaderValue(authorizationHeaderValue)) {
             chain.doFilter(request, response);
             return;
         }
 
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = getAuthenticationToken(request);
+        final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = getAuthenticationToken(request);
+
         SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
         chain.doFilter(request, response);
     }
@@ -41,18 +46,22 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     private UsernamePasswordAuthenticationToken getAuthenticationToken(HttpServletRequest request) {
         String token = request.getHeader(HEADER_NAME);
 
-        if (token == null) {
-            return null;
+        if (Objects.isNull(token)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Request should contain header with token.");
         }
 
-        String username = Jwts.parser().setSigningKey(SECRET)
+        final String username = Jwts.parser().setSigningKey(SECRET)
                 .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
                 .getBody()
                 .getSubject();
 
-        UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
-        User user = customUserDetailService.loadCustomUserByUsername(username);
-        return userDetails != null ? new UsernamePasswordAuthenticationToken(user, null, userDetails.getAuthorities()) : null;
+        UserDetails userDetails = contractorsUserDetailService.loadUserByUsername(username);
+        User user = contractorsUserDetailService.loadCustomUserByUsername(username);
 
+        return new UsernamePasswordAuthenticationToken(user, null, userDetails.getAuthorities());
+    }
+
+    private boolean hasInvalidHeaderValue(String authorizationHeaderValue) {
+        return Objects.isNull(authorizationHeaderValue) || !authorizationHeaderValue.startsWith(TOKEN_PREFIX);
     }
 }
