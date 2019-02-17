78b1a2cd5494a03d9b595b8a18fce9af91dafcba
Oleg Barmin <olegbarmin@gmail.com>
2019-02-17 23:09:37 +0200

    Add singUp endpoint.

--- contractors/src/main/java/com/university/contractors/config/Endpoints.java
+++ contractors/src/main/java/com/university/contractors/config/Endpoints.java
@@ -2,9 +2,13 @@ package com.university.contractors.config;
 
 public interface Endpoints {
 
-    String STUDENTS = "/students";
+    String API_PREFIX = "/api";
+    String ENTITY_PREFIX = API_PREFIX + "/entity";
+
+    String STUDENTS = ENTITY_PREFIX + "/students";
     String STUDENT_BY_ID = STUDENTS + "/{id}";
 
-    String LOGIN = "/login";
+    String LOGIN = API_PREFIX + "/login";
+    String SIGN_UP = API_PREFIX + "/signUp";
 
 }
