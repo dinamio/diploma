056bdb82ea139e862ce9eb1eb87cb76c8ba3ca2e
Oleg Barmin <olegbarmin@gmail.com>
2019-02-17 23:35:16 +0200

    Add SignUpUser payload, to work with SingUpRequest.

---
+++ contractors/src/main/java/com/university/contractors/controller/payload/SignUpUser.java
@@ -0,0 +1,32 @@
+package com.university.contractors.controller.payload;
+
+public class SignUpUser {
+
+    private String username;
+    private String password;
+    private String confirmationPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmationPassword() {
+        return confirmationPassword;
+    }
+
+    public void setConfirmationPassword(String confirmationPassword) {
+        this.confirmationPassword = confirmationPassword;
+    }
+}
