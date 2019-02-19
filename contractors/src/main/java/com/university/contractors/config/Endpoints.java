31cc9eb6fd587b975ec3fd6e2aee7075a407ceb2
Oleg Barmin <olegbarmin@gmail.com>
2019-02-20 00:31:43 +0200

    Add version to all api endpoints.

--- contractors/src/main/java/com/university/contractors/config/Endpoints.java
+++ contractors/src/main/java/com/university/contractors/config/Endpoints.java
@@ -3,12 +3,14 @@ package com.university.contractors.config;
 public interface Endpoints {
 
     String API_PREFIX = "/api";
-    String ENTITY_PREFIX = API_PREFIX + "/entity";
+    String API_VERSION_PREFIX = "/v1";
+    String ENDPOINTS_PREFIX = API_PREFIX + API_VERSION_PREFIX;
+    String ENTITY_PREFIX = ENDPOINTS_PREFIX + "/entity";
 
     String STUDENTS = ENTITY_PREFIX + "/students";
     String STUDENT_BY_ID = STUDENTS + "/{id}";
 
-    String LOGIN = API_PREFIX + "/login";
-    String SIGN_UP = API_PREFIX + "/signUp";
+    String LOGIN = ENDPOINTS_PREFIX + "/login";
+    String SIGN_UP = ENDPOINTS_PREFIX + "/signUp";
 
 }
