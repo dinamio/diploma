3aa45279ebb0fdb7e8991e2b25dab954c43d2f6f
Oleg Barmin <olegbarmin@gmail.com>
2019-02-24 21:27:03 +0200

    Replace "should" with "must" in exception messages.

--- contractors/src/main/java/com/university/contractors/service/MalformedTokenException.java
+++ contractors/src/main/java/com/university/contractors/service/MalformedTokenException.java
@@ -5,7 +5,7 @@ import com.university.contractors.config.SecurityConstants;
 public class MalformedTokenException extends RuntimeException {
 
     public MalformedTokenException() {
-        super(String.format("Token should start with '%s' prefix and contain authorization token, " +
+        super(String.format("Token must start with '%s' prefix and contain authorization token, " +
                 "separated from the prefix with single space.", SecurityConstants.TOKEN_PREFIX));
     }
 }
