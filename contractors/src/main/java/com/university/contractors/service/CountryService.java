84a0f10d31063f3379573f34ce7f3b6b9328b275
Oleg Barmin <olegbarmin@gmail.com>
2019-03-17 21:36:29 +0200

    Use 'RuntimeException' instead of 'InvalidStateException'.

--- contractors/src/main/java/com/university/contractors/service/CountryService.java
+++ contractors/src/main/java/com/university/contractors/service/CountryService.java
@@ -4,7 +4,6 @@ import com.university.contractors.model.Country;
 import com.university.contractors.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.plugin.dom.exception.InvalidStateException;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +25,7 @@ public class CountryService {
         final List<Country> conuntryList = countryRepository.findByCountryNameEng(LOCAL_COUNTRY_ENG_NAME);
 
         if (conuntryList.size() > 1) {
-            throw new InvalidStateException("There more than one country with name: '" + LOCAL_COUNTRY_ENG_NAME + "'");
+            throw new RuntimeException("There more than one country with name: '" + LOCAL_COUNTRY_ENG_NAME + "'");
         }
 
         final Optional<Country> optionalCountry = conuntryList.stream().findFirst();
